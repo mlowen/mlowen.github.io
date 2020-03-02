35bc60075836ac562185ad50c850aa5027458df8
Mike Lowen <mike.lowen@lic.co.nz>
2020-03-03 10:18:07 +1300

    Updating dependencies
    Updated the Gemfile.lock to take care of the security notification
    from github regarding nokogiri. Updated the vagrant file to only
    create a VM with 512mb of ram as 2gb was excessive for a static
    site. Tried to go lower but found that the VM was blowing up while
    trying to set it up. Updated the cookbook for the VM so that it does
    not install foreman by default when installing ruby as it is not
    needed to run the site.

--- cookbooks/devenv/recipes/default.rb
+++ cookbooks/devenv/recipes/default.rb
@@ -1,10 +1,9 @@
-package 'gpgv2'
-package 'git'
+# package 'gpgv2'
 
 # Ruby
 execute 'curl -sSL https://rvm.io/mpapis.asc | gpg --import -'
 execute 'curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -'
-execute 'curl -sSL https://raw.githubusercontent.com/rvm/rvm/stable/binscripts/rvm-installer | bash -s stable --ruby=2.6.3 --gems=foreman'
+execute 'curl -sSL https://raw.githubusercontent.com/rvm/rvm/stable/binscripts/rvm-installer | bash -s stable --ruby=2.6.3'
 
 group 'rvm' do
   action :manage
